98dbe66541fe34d759bdcd66596205e2c7b811d5
Enrico <Enrico.Huettig@kontron-ais.com>
2024-08-30 18:55:53 +0200

    update to .NET 8

--- ComputerUpTime/Program.cs
+++ ComputerUpTime/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Versioning;
+using System.Xml;
 
 namespace ComputerUpTime;
 
@@ -8,11 +10,23 @@ namespace ComputerUpTime;
 [ExcludeFromCodeCoverage]
 internal class Program
 {
+    [SupportedOSPlatform("windows")]
     private static void Main(string[] _)
     {
         var log = new EventLog("System");
+        var desiredInstanceIds =  new List<long>
+        {
+            42,  // Sleep
+            507, // Wake Up
+            13,  // Shutdown
+            12   // Started
+        };
+
         var mapper = new ActivityMapper(
-            log.Entries.Cast<EventLogEntry>().Select(entry => new WorkDayActivity(entry.TimeGenerated)),
+            log.Entries
+                .Cast<EventLogEntry>()
+                .Where(entry => desiredInstanceIds.Contains(entry.InstanceId))
+                .Select(entry => new WorkDayActivity(entry.TimeGenerated)),
             new SystemTime(),
             new WorkDayLogger());
 
